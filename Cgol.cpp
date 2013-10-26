c6a8ecc48c5da06119a1556cce43c892a5b9109d
Jacob Hughes <exrook.j@gmail.com>
2013-10-26 18:10:17 -0400

    Now acurately simulates Conways game of life using Cgol.cpp Added  .gitignore

--- Cgol.cpp
+++ Cgol.cpp
@@ -2,13 +2,23 @@
 
 char* Cgol::compute(const int sizeX, const int sizeY, char data[]) {
   int po = 0;
-  int nb = 0;
+  int nc = 0;
   char *out = new char[sizeX*sizeY];
   for(int i = 1; i < (sizeX-1); i++) {
     for(int j = 1; j < (sizeY-1); j++) {
       po = i*sizeY+j;
-      nb = data[po+1] + data[po-1] + data[po+sizeY] + data[po-sizeY];
-      out[po] = (nb & 2);
+      nc = data[(po-1)-sizeY] + data[po-sizeY] + data[(po+1)-sizeY]\
+        + data[po-1] + data[po+1]\
+        + data[(po-1)+sizeY] + data[po+sizeY] + data[(po+1)+sizeY];
+      if (nc < 2)
+        out[po] = 0;
+      else if ((nc <= 3) && (data[po] != 0))
+        out[po] = 1;
+      else if (nc != 3)
+        out[po] = 0;
+      else
+        out[po] = 1;
+      //out[po] = nc;
     }
   }
   return out;
