175fb14f9eddf94906fa8f7f7095dba67bd522f7
Jacob Hughes <exrook.j@gmail.com>
2013-10-27 03:00:26 -0400

    Can now read an initial starting state from stdin, example format is in test Added option -d (demo) to use an internal 10x10 grid

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <chrono>
 #include <thread>
 #include <string>
+#include <sstream>
+#include <vector>
 #include "Rule.h"
 #include "Board.h"
 #include "Cgol.h"
@@ -18,10 +20,13 @@ char init[10][10] = \
 {1,1,1,0,0,0,0,0,0,0},
 {0,0,0,0,0,0,0,0,0,0}};
 
+char* parse(std::istream &in, int &w, int &h);
+
 int main(int argc, char *argv[]) {
   bool pretty = false;
-  int loop = 10;
   int prettyt = 1000;
+  int loop = 10;
+  bool demo = false;
   for(int i=1; i < argc; i++) {
     if (std::string(argv[i]) == "-p") {
       pretty = true;
@@ -35,10 +40,16 @@ int main(int argc, char *argv[]) {
         std::cout << "Invalid Options\n";
         return 0;
       }
+    } else if (std::string(argv[i]) == "-d") {
+      demo = true;
     }
   }
   
-  Board lol(10,10,(char**)&init);
+  int w = 10, h=10;
+  char **lolp = (char**)&init;
+  if (!demo)
+    lolp = (char**)parse(std::cin,w,h);
+  Board lol(w,h,lolp);
   if (pretty) lol.print();
   Rule *sd = new Cgol();
   for(int i = 0; i < loop; i++) {
@@ -50,4 +61,27 @@ int main(int argc, char *argv[]) {
     }
   }
   if (!pretty) lol.print();
+}
+
+char* parse(std::istream &in, int &w, int &h) {
+  std::vector<char> data;
+  char l;
+  char *out;
+  w = 0;
+  h = 0;
+
+  while (!in.eof()) {
+    std::string inl;
+    getline(in,inl);
+    std::istringstream ins(inl);
+    while (ins >> l) {
+      if (l != ','||l != ' ') {
+        data.push_back(l == '#' ? 1:0);
+        w += (!h ? 1:0);
+      }
+    }
+    h++;
+  }
+  h--;
+  return &data[0]; 
 }
